ee985ecd43d003934b299010de09085f9fdf2cb2
Jan Grosser <email@jan-grosser.de>
2014-08-16 21:04:03 +0200

    Code refactoring, mainly translation

--- bootloader/write_buffer.s
+++ bootloader/write_buffer.s
@@ -2,22 +2,21 @@
 #include <avr/io.h>
 #include "asm_include.h"
 
-	.section .text
-	.global write_buffer
+    .section .text
+    .global write_buffer
 
-/********************************
-	speichert den empfangspuffer
-	in der aktuellen flash-seite
-*********************************/
+/******************************************************************************
+    Stores the received buffer to current flash page.
+******************************************************************************/
 write_buffer:
-		lds		ZL, page_adr					// flash address
-		lds		ZH, page_adr+1					// gültige page
-		ldi		YH, hi8(flash_buffer)			// ram-pointer
-		ldi		YL, lo8(flash_buffer)			// address
-		rcall	flash_write_puffer				// ram-parameter ins flash schreiben
-		ldi		temp1, -1						// für übertrag
-		subi	ZL, -SPM_PAGESIZE				// nächste seite im flash berechnen
-		sbc		ZH, temp1						// -borrow
-		sts		page_adr, ZL					// aktuelle flash-adresse 
-		sts		page_adr+1, ZH					// wieder abspeichern
-		ret										// zurück
+    lds     ZL, page_adr                    // flash address
+    lds     ZH, page_adr+1                  // valid page
+    ldi     YH, hi8(flash_buffer)           // RAM pointer
+    ldi     YL, lo8(flash_buffer)           // address
+    rcall   flash_write_puffer              // write RAM parameter to flash
+    ldi     temp1, -1                       // (fuer uebertrag?)
+    subi    ZL, -SPM_PAGESIZE               // compute next page in flash
+    sbc     ZH, temp1                       // -borrow
+    sts     page_adr, ZL                    // current flash address 
+    sts     page_adr+1, ZH                  // save again
+    ret
