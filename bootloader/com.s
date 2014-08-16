3e6ae66e0514049c3f1a23f2417c1ba49573bd47
Jan Grosser <email@jan-grosser.de>
2014-08-16 20:23:23 +0200

    Code refactoring, mainly translation

--- bootloader/com.s
+++ bootloader/com.s
@@ -3,60 +3,51 @@
 #include <compat/twi.h>
 #include "asm_include.h"
 
-            .section .text
+    .section .text
+    .global	com_get
+    .global com_rdy
+    .global com_put
+    .global com_put_string
 
-			.global	com_get
-			.global com_rdy
-			.global com_put
-			.global com_put_string
-
-/************************************
-	hole daten vom seriellen port
-*************************************/
+/******************************************************************************
+	Read date from serial port
+******************************************************************************/
 com_get:
-		sbis	IO_REG(UCSRA), RXC
-		rjmp	com_get
-		in		CHAR_GET_REG, IO_REG(UDR)
-		ret
+    sbis    IO_REG(UCSRA), RXC
+    rjmp    com_get
+    in      CHAR_GET_REG, IO_REG(UDR)
+    ret
 
-/***********************************
-	teste ob daten im datenregister
-************************************/
+/******************************************************************************
+    Test, if data is in the buffer
+******************************************************************************/
 com_rdy:
-		sbis	IO_REG(UCSRA), RXC
-		rjmp	no_data
-		in		CHAR_GET_REG, IO_REG(UDR)		// daten im datenregister
+    sbis    IO_REG(UCSRA), RXC
+    rjmp    no_data
+    in      CHAR_GET_REG, IO_REG(UDR)       // data present in buffer
 com_rdy_ret:
-		ret	
+    ret
 no_data:
-		eor		CHAR_GET_REG, CHAR_GET_REG		// keine daten im datenregister
-		rjmp 	com_rdy_ret
+    eor     CHAR_GET_REG, CHAR_GET_REG      // no data
+    rjmp    com_rdy_ret
 
-/*******************************************
-	sende datenbyte über den seriellen port
-********************************************/
+/******************************************************************************
+    Send data over serial port
+******************************************************************************/
 com_put:
-		sbis	IO_REG(UCSRA), UDRE
-		rjmp	com_put							// noch daten im sendepuffer
-		out		IO_REG(UDR), CHAR_PUT_REG		// sende daten
-		ret
+    sbis    IO_REG(UCSRA), UDRE
+    rjmp    com_put                         // data left in buffer
+    out     IO_REG(UDR), CHAR_PUT_REG       // send data
+    ret
 
-/******************************************
-	sende daten-string aus dem datenpuffer
-*******************************************/
+/******************************************************************************
+    Send the data string in data buffer Z 
+******************************************************************************/
 com_put_string:
-		ld		CHAR_PUT_REG, Z+
-		tst		CHAR_PUT_REG
-		breq	com_send_end
-		rcall	com_put
-		rjmp	com_put_string
+    ld      CHAR_PUT_REG, Z+
+    tst     CHAR_PUT_REG
+    breq    com_send_end
+    rcall   com_put
+    rjmp    com_put_string
 com_send_end:
-		ret
-
-
-
-
-
-
-
-
+    ret
