90d87a7be3b064b597d5d5d22765df5b985492ea
Jan Grosser <email@jan-grosser.de>
2014-08-16 20:52:58 +0200

    Code refactoring, mainly translation

--- bootloader/init_asuro_com.s
+++ bootloader/init_asuro_com.s
@@ -2,27 +2,32 @@
 #include <avr/io.h>
 #include "asm_include.h"
 
-	.section .text
-	.global init_asuro_com
+    .section .text
+    .global init_asuro_com
 
 /*
-	initialisiert die notwendigen module für den asuro
+    Initialize the modules needed by the Asuro robot.
 */
 init_asuro_com:
+/*
+    36khz ir-clk with timer2
+*/
+    ldi     temp1, BIT(WGM21) + BIT(COM20) + BIT(CS20)
+    out     IO_REG(TCCR2), temp1
+    ldi     temp1, 0x6E
+    out     IO_REG(OCR2), temp1
 
-	ldi 	temp1, BIT(WGM21) + BIT(COM20) + BIT(CS20)	// 36khz ir-clk mit timer2
-	out		IO_REG(TCCR2), temp1
-	ldi		temp1, 0x6E
-	out		IO_REG(OCR2), temp1
+/*
+    Initialize ADC. ADC clock = system clock / 64
+*/
+    ldi     temp1, BIT(ADEN) + BIT(ADPS2) + BIT(ADPS1)
+    out     IO_REG(ADCSRA), temp1           // adc init
 
-	ldi		temp1, BIT(ADEN) + BIT(ADPS2) + BIT(ADPS1)	// adc-clk = sys-clk/64 
-	out		IO_REG(ADCSRA), temp1						// adc init
+    ldi     temp1, BIT(PB3) + BIT(PB0)
+    out     IO_REG(DDRB), temp1             // ir-led-driver and status led1
+    ldi     temp1, BIT(PD2)                 // status led2							
+    out     IO_REG(DDRD), temp1
 
-	ldi		temp1, BIT(PB3) + BIT(PB0)
-	out		IO_REG(DDRB), temp1							// ir-led-driver und status-led1
-	ldi		temp1, BIT(PD2)								// status-led2							
-	out		IO_REG(DDRD), temp1
-
-	sbi		IO_REG(DDRD), PD6							// rote front-led für batterie
-	ret
+    sbi     IO_REG(DDRD), PD6               // red front led for battery
+    ret
 
