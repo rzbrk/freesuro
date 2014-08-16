dc7aa00114203c1e91eba38c58bb0901e56f6b73
Jan Grosser <email@jan-grosser.de>
2014-08-16 20:09:45 +0200

    Code refactoring, mainly translation

--- bootloader/asm_include.h
+++ bootloader/asm_include.h
@@ -5,24 +5,26 @@
 #define sbi(p,b) p|=(1<<(b))
 #define cbi(p,b) p&=(~(1<<(b)))
 
-/*************************
-	registerdefinitionen
-**************************/
-#define CHAR_GET_REG r24	// register für char-rückgabe 
-#define CHAR_PUT_REG r25	// register für char-übergabe
-#define CHAR_RET_REG r24	// register für char-rückgabe 
+/******************************************************************************
+    register definitions
+******************************************************************************/
+#define CHAR_GET_REG r24                // register for returning char 
+#define CHAR_PUT_REG r25                // register for returning char
+#define CHAR_RET_REG r24                // register for returning char
 
-#define INT_REG_H r25		// register für int high byte
-#define INT_REG_L r24		// register für int low  byte
+#define INT_REG_H r25                   // register for int high byte
+#define INT_REG_L r24                   // register for int low  byte
 
-#define temp1 r16			// arbeitsregister
-#define temp2 r17
-#define temp3 r18
-#define temp4 r19
+#define temp1 r16                       // working register
+#define temp2 r17                       // working register
+#define temp3 r18                       // working register
+#define temp4 r19                       // working register
 
-#define STARTZEICHEN ':'	// startzeichen für einen record
-#define SEKUNDE 2			// für zeitschleife
+#define STARTZEICHEN ':'                // start char for a record within a
+                                        // *.hex file
+#define SEKUNDE 2                       // for loop
 
-#define MAXRECORDS 17		// wert für maximale anzahl daten im record
+#define MAXRECORDS 17                   // maximum number of data in record
 
-#define BATT_MIN 770  		// minimalster batterie-wert ca. 4,2V
+#define BATT_MIN 770                    // lower battery treshold,
+                                        // approx. 4,2V
