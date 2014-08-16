ba1327ae05783da71ceb14e9506625e76d345a34
Jan Grosser <email@jan-grosser.de>
2014-08-16 20:46:49 +0200

    Code refactoring, mainly translation

--- bootloader/global_var.c
+++ bootloader/global_var.c
@@ -5,15 +5,17 @@
 volatile unsigned char flash_buffer[SPM_PAGESIZE];
 volatile unsigned char int_buffer[3];
 
-volatile unsigned char wait_send[]={"\n\r\n\r(c)Arexx Netherlands [BL-V1.03] / Wait...\n\r"};
-volatile unsigned char flash_ok[]={"\n\rData transfer successfully!"};
+#volatile unsigned char wait_send[]={"\n\r\n\r(c)Arexx Netherlands [BL-V1.03] / Wait...\n\r"};
+volatile unsigned char wait_send[]
+    = {"\n\r\n\rFreesuro Bootloader / Wait...\n\r"};
+volatile unsigned char flash_ok[] = {"\n\rData transfer successfully!"};
 
-volatile unsigned int  page_adr;	// flashpage-adresse
-volatile unsigned char check_sum;	// chechsum für den record
+volatile unsigned int  page_adr;            // flashpage address
+volatile unsigned char check_sum;           // chechsum for record of hex file
 
-volatile unsigned char rec_count;	// anzahl der byte im record
-volatile unsigned char ist_count;	// anzahl der aktuell eingelesenen byte
+volatile unsigned char rec_count;           // no of byte in record
+volatile unsigned char ist_count;           // no of byte read in
 
-volatile unsigned char flash_count;	// anzahl der eingelesenen byte im flash-puffer
-volatile unsigned int  puffer_adr;	// aktulle adresse vom flash-puffer
+volatile unsigned char flash_count;         // no of byte in flash buffer
+volatile unsigned int  puffer_adr;          // current address of flash buffer
 
