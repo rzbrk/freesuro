40ca3c7eafcfaa891581dda8e7b572aecde51ff6
Jan Grosser <email@jan-grosser.de>
2014-08-16 20:02:17 +0200

    Code refactoring, mainly translation

--- bootloader/ascii_hex.s
+++ bootloader/ascii_hex.s
@@ -2,47 +2,49 @@
 #include <avr/io.h>
 #include "asm_include.h"
 
-	.section .text
-	.global ascii_hex
+    .section .text
+    .global ascii_hex
 
-/*********************************************
-	wandelt eine ascii-hex-zahl
-	aus zwei byte in einen hex-char-wert
-	rückgabe in INT_REG_L
-	in der hex-datei sind nur grossbuchstaben
-**********************************************/
+/******************************************************************************
+    Converts numbers in ascii hex string (2 bytes) into a "real" hex
+    number (1 byte), e.g.:
+    
+    "1A" (2 bytes) --> 0x1a (1 byte)
+    
+    The return value is stored in INT_REG_L.
+    
+    Note: The ascii hex string only contains upper case characters.
+******************************************************************************/
 ascii_hex:
 
-	ldi		ZL, lo8(int_buffer)			// zeiger auf zeichenkette			
-	ldi		ZH, hi8(int_buffer)
+    ldi     ZL, lo8(int_buffer)             // pointer to string			
+    ldi     ZH, hi8(int_buffer)
 
-	ld		temp1, Z+					// erstes zeichen
-	rcall	hex_value					// umwandeln
+    ld      temp1, Z+                       // first character
+    rcall   hex_value                       // convert
 
-	ldi		temp2, 16					// mit 16
-	mul		temp1, temp2				// multiplizieren ergebnis in r0:r1
+    ldi     temp2, 16                       // multiply with 16
+    mul     temp1, temp2                    // ... and move result to r0:r1
 
-	ld		temp1, Z+					// zweites zeichen
-	rcall	hex_value					// umwandeln
-	mov		INT_REG_L, r0				// ergebnis bilden aus byte1 *16
-	add		INT_REG_L, temp1			// plus rest aus byte2
+    ld      temp1, Z+                       // second character
+    rcall   hex_value                       // convert
+    mov     INT_REG_L, r0                   // compute result from byte1 *16
+    add     INT_REG_L, temp1                // ... and add the rest from byte2
 
-	ret
+    ret
 
 /**************************
-	ascii-hex-byte in zahl
+    one ascii-hex-byte --> number
 ***************************/
 hex_value:
-	cpi		temp1, 'A'					// ascii '0'-'9'?
-	brlo	next_hex					// ja
-	subi	temp1, 'A'					// nein
-	subi	temp1, -10
-	rjmp	hex_end
+    cpi     temp1, 'A'                      // ascii '0'-'9'?
+    brlo    next_hex                        //  yes
+    subi    temp1, 'A'                      //  no
+    subi    temp1, -10
+    rjmp    hex_end
 
 next_hex:
-	subi	temp1, '0'
+    subi    temp1, '0'
 
 hex_end:
-	ret
-
-
+    ret
